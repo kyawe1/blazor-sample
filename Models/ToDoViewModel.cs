39fcd0d089476cf09fe4bc6d6640e7be01024c94
kyawe1 <kk09254010314@gmail.com>
2021-12-21 16:11:21 +0630

    first

---
+++ Models/ToDoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo.Models
+{
+    public class ToDoViewModel
+    {
+        public int Id { set; get; }
+        public int userId { set; get; }
+        public string? title { set; get; }
+        public bool completed { set; get; }
+    }
+}
---
+++ ToDoStore.cs
@@ -0,0 +1,30 @@
+﻿using ToDo.Models;
+namespace ToDo
+{
+    public class ToDoStore
+    {
+        private List<ToDoViewModel> toDos { set; get; } 
+        public ToDoStore()
+        {
+            toDos = new List<ToDoViewModel>();
+        }
+        public void Add(ToDoViewModel t)
+        {
+            toDos.Add(t);
+            
+        }
+        public int Count()
+        {
+            return this.toDos.Count;
+        }
+        public List<ToDoViewModel> getStore()
+        {
+            return this.toDos;
+        }
+        public bool Remove(ToDoViewModel item)
+        {
+            return toDos.Remove(item);
+            
+        }
+    }
+}

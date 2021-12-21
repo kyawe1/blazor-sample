623bf7de2367dc0264002677465e300cfe735fea
kyawe1 <kk09254010314@gmail.com>
2021-12-21 23:52:19 +0630

    a

--- Models/ToDoViewModel.cs
+++ Models/ToDoViewModel.cs
@@ -5,6 +5,6 @@
         public int Id { set; get; }
         public int userId { set; get; }
         public string? title { set; get; }
-        public bool completed { set; get; }
+        public bool completed { set; get; }=false;
     }
 }
